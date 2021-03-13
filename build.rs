e09544aee5f778b274169d091599346f511bb121
David Koloski <djkoloski@gmail.com>
2021-03-13 11:27:13 -0600

    Configure out capnp path for non-windows targets

--- build.rs
+++ build.rs
@@ -1,31 +1,23 @@
-fn main() {
-    capnpc::CompilerCommand::new()
-        .capnp_executable("tools/capnp.exe")
-        .file("src/datasets/log/log.capnp")
-        .output_path(".")
-        .default_parent_module(vec!["datasets".into(), "log".into()])
-        .run()
-        .unwrap();
-    capnpc::CompilerCommand::new()
-        .capnp_executable("tools/capnp.exe")
-        .file("src/datasets/mesh/mesh.capnp")
-        .output_path(".")
-        .default_parent_module(vec!["datasets".into(), "mesh".into()])
-        .run()
-        .unwrap();
-    capnpc::CompilerCommand::new()
-        .capnp_executable("tools/capnp.exe")
-        .file("src/datasets/minecraft_savedata/minecraft_savedata.capnp")
-        .output_path(".")
-        .default_parent_module(vec!["datasets".into(), "minecraft_savedata".into()])
-        .run()
-        .unwrap();
+fn capnpc_compile_dataset(name: &'static str) -> capnp::Result<()> {
+    let mut command = capnpc::CompilerCommand::new();
+    #[cfg(windows)]
+    command.capnp_executable("tools/capnp.exe");
+    command.file(&format!("src/datasets/{0}/{0}.capnp", name));
+    command.output_path(".");
+    command.default_parent_module(vec!["datasets".into(), name.into()]);
+    command.run()
+}
+
+fn prost_compile_dataset(name: &'static str) -> std::io::Result<()> {
     let mut prost_config = prost_build::Config::new();
     prost_config.protoc_arg("--experimental_allow_proto3_optional");
-    prost_config.compile_protos(&["src/datasets/log/log.proto"], &["src"])
-        .unwrap();
-    prost_config.compile_protos(&["src/datasets/mesh/mesh.proto"], &["src"])
-        .unwrap();
-    prost_config.compile_protos(&["src/datasets/minecraft_savedata/minecraft_savedata.proto"], &["src"])
-        .unwrap();
+    prost_config.compile_protos(&[format!("src/datasets/{0}/{0}.proto", name).as_str()], &["src"])
+}
+
+fn main() {
+    const DATASETS: [&'static str; 3] = ["log", "mesh", "minecraft_savedata"];
+    for &name in DATASETS.iter() {
+        capnpc_compile_dataset(name).unwrap();
+        prost_compile_dataset(name).unwrap();
+    }
 }
