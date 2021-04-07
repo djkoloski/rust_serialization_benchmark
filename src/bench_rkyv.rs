7a3174d0bb1df2b0cfabb71dcd94f4c126f2f57d
David Koloski <djkoloski@gmail.com>
2021-04-07 11:44:28 -0500

    Update rkyv to 0.5.1, use AlignedSerializer

--- src/bench_rkyv.rs
+++ src/bench_rkyv.rs
@@ -4,9 +4,11 @@ use criterion::{black_box, Criterion};
 use rkyv::{
     archived_value,
     archived_value_mut,
+    check_archived_value,
     de::deserializers::AllocDeserializer,
-    ser::{Serializer, serializers::WriteSerializer},
-    validation::{check_archive, DefaultArchiveValidator},
+    ser::{Serializer, serializers::AlignedSerializer},
+    validation::{DefaultArchiveValidator},
+    AlignedVec,
     Archive,
     Deserialize,
     Serialize,
@@ -14,7 +16,7 @@ use rkyv::{
 
 pub fn bench<T, R, U>(name: &'static str, c: &mut Criterion, data: &T, read: R, update: U)
 where
-    T: Archive + Serialize<WriteSerializer<Vec<u8>>> + for<'a> Serialize<WriteSerializer<&'a mut [u8]>>,
+    T: Archive + for<'a> Serialize<AlignedSerializer<&'a mut AlignedVec>>,
     T::Archived: CheckBytes<DefaultArchiveValidator> + Deserialize<T, AllocDeserializer>,
     R: Fn(&T::Archived),
     U: Fn(Pin<&mut T::Archived>),
@@ -23,19 +25,21 @@ where
 
     let mut group = c.benchmark_group(format!("{}/rkyv", name));
 
-    let mut serialize_buffer = vec![0u8; BUFFER_LEN];
+    let mut serialize_buffer = AlignedVec::with_capacity(BUFFER_LEN);
     group.bench_function("serialize", |b| {
         b.iter(|| {
-            let mut serializer = WriteSerializer::new(black_box(serialize_buffer.as_mut_slice()));
+            serialize_buffer.clear();
+            let mut serializer = AlignedSerializer::new(black_box(&mut serialize_buffer));
             black_box(
                 serializer.serialize_value(black_box(data)).unwrap()
             );
         })
     });
 
-    let mut serializer = WriteSerializer::new(Vec::new());
+    serialize_buffer.clear();
+    let mut serializer = AlignedSerializer::new(&mut serialize_buffer);
     let pos = serializer.serialize_value(data).unwrap();
-    let mut deserialize_buffer = serializer.into_inner();
+    let deserialize_buffer = serializer.into_inner();
 
     group.bench_function("access (unvalidated)", |b| {
         b.iter(|| {
@@ -48,7 +52,7 @@ where
     group.bench_function("access (validated)", |b| {
         b.iter(|| {
             black_box(
-                check_archive::<T>(black_box(deserialize_buffer.as_ref()), black_box(pos)).unwrap()
+                check_archived_value::<T>(black_box(deserialize_buffer.as_ref()), black_box(pos)).unwrap()
             );
         })
     });
@@ -64,7 +68,7 @@ where
     group.bench_function("read (validated)", |b| {
         b.iter(|| {
             black_box(
-                read(check_archive::<T>(black_box(deserialize_buffer.as_ref()), black_box(pos)).unwrap())
+                read(check_archived_value::<T>(black_box(deserialize_buffer.as_ref()), black_box(pos)).unwrap())
             );
         })
     });
