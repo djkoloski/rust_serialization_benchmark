2bae7fd305efdd75962f316ac53db575c16b1562
8192K <sebastian@frehmel.de>
2024-01-06 10:45:51 +0100

    All bincode2 benchmarks running

--- src/bench_bincode2.rs
+++ src/bench_bincode2.rs
@@ -7,35 +7,30 @@ where
     const BUFFER_LEN: usize = 10_000_000;
     let mut group = c.benchmark_group(format!("{}/bincode2", name));
 
-    let mut serialize_buffer = vec![0; BUFFER_LEN];
+    let mut buffer = Box::new([0u8; BUFFER_LEN]);
     let conf = bincode::config::standard();
     group.bench_function("serialize", |b| {
         b.iter(|| {
-            bincode::encode_into_slice(
-                black_box(&data),
-                black_box(serialize_buffer.as_mut_slice()),
-                conf,
-            )
-            .unwrap();
-            black_box(());
+            let size = bincode::encode_into_slice(black_box(&data), black_box(&mut *buffer), conf)
+                .unwrap();
+            black_box(&buffer[..size]);
         })
     });
 
-    let deserialize_buffer = serialize_buffer.as_mut_slice();
-    let size = bincode::encode_into_slice(&data, deserialize_buffer, conf).unwrap();
-    let deserialize_buffer = &deserialize_buffer[..size];
+    let size = bincode::encode_into_slice(&data, &mut *buffer, conf).unwrap();
+    let buffer = &buffer[..size];
 
     group.bench_function("deserialize", |b| {
         b.iter(|| {
             black_box(
-                bincode::decode_from_slice::<T, _>(black_box(deserialize_buffer), conf)
+                bincode::decode_from_slice::<T, _>(black_box(buffer), conf)
                     .unwrap()
                     .0,
             );
         })
     });
 
-    crate::bench_size(name, "bincode2", deserialize_buffer);
+    crate::bench_size(name, "bincode2", buffer);
 
     group.finish();
 }
