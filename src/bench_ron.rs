e9de5391f0573d58854ea5b5cb7492470d40613f
Kent Ross <k@mad.cash>
2025-06-06 11:17:22 -0700

    update ron, which writes Strings now

--- src/bench_ron.rs
+++ src/bench_ron.rs
@@ -10,27 +10,27 @@ where
 
     let mut group = c.benchmark_group(format!("{}/ron", name));
 
-    let mut serialize_buffer = vec![0; BUFFER_LEN];
+    let mut serialize_buffer = String::with_capacity(BUFFER_LEN);
     group.bench_function("serialize", |b| {
         b.iter(|| {
-            ron::ser::to_writer(black_box(serialize_buffer.as_mut_slice()), black_box(&data))
+            ron::ser::to_writer(black_box(&mut serialize_buffer), black_box(&data))
                 .unwrap();
             black_box(());
         })
     });
 
-    let mut deserialize_buffer = Vec::new();
+    let mut deserialize_buffer = String::new();
     ron::ser::to_writer(&mut deserialize_buffer, &data).unwrap();
 
     group.bench_function("deserialize", |b| {
         b.iter(|| {
-            black_box(ron::de::from_bytes::<'_, T>(black_box(&deserialize_buffer)).unwrap());
+            black_box(ron::de::from_bytes::<'_, T>(black_box(deserialize_buffer.as_ref())).unwrap());
         })
     });
 
-    crate::bench_size(name, "ron", deserialize_buffer.as_slice());
+    crate::bench_size(name, "ron", deserialize_buffer.as_ref());
 
-    assert!(ron::de::from_bytes::<T>(&deserialize_buffer).unwrap() == *data);
+    assert!(ron::de::from_bytes::<T>(deserialize_buffer.as_ref()).unwrap() == *data);
 
     group.finish();
 }
@@ -44,22 +44,22 @@ where
 
     let mut group = c.benchmark_group(format!("{}/ron", name));
 
-    let mut deserialize_buffer = Vec::new();
+    let mut deserialize_buffer = String::new();
     ron::ser::to_writer(&mut deserialize_buffer, &data).unwrap();
     let bdata = T::Borrowed::from(data);
 
     // The borrowed variant type should encode exactly the same as the owned type.
-    let mut borrowed_buffer = Vec::new();
+    let mut borrowed_buffer = String::new();
     ron::ser::to_writer(&mut borrowed_buffer, &bdata).unwrap();
     assert_eq!(borrowed_buffer, deserialize_buffer);
 
     // The borrowed value we decode should be equivalent to the input
-    assert!(ron::de::from_bytes::<T::Borrowed<'_>>(&deserialize_buffer).unwrap() == bdata);
+    assert!(ron::de::from_bytes::<T::Borrowed<'_>>(deserialize_buffer.as_ref()).unwrap() == bdata);
 
     group.bench_function("borrow", |b| {
         b.iter(|| {
             black_box(
-                ron::de::from_bytes::<T::Borrowed<'_>>(black_box(&deserialize_buffer)).unwrap(),
+                ron::de::from_bytes::<T::Borrowed<'_>>(black_box(deserialize_buffer.as_ref())).unwrap(),
             );
         })
     });
