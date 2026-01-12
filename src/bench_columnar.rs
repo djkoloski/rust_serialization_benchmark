b63ad038d4976f9059188022f2240d6ff33d04c9
Frank McSherry <fmcsherry@me.com>
2026-01-11 19:41:43 -0500

    cargo fml

--- src/bench_columnar.rs
+++ src/bench_columnar.rs
@@ -1,8 +1,11 @@
+use columnar::{
+    bytemuck,
+    bytes::{EncodeDecode, Indexed},
+    Borrow, Clear, Columnar, FromBytes, Index, Push,
+};
 use criterion::{black_box, Criterion};
-use columnar::{bytemuck, Columnar, Borrow, FromBytes, Index, Push, Clear, bytes::{EncodeDecode, Indexed}};
 
-pub fn bench<T: Columnar+PartialEq>(name: &'static str, c: &mut Criterion, data: &T) {
-
+pub fn bench<T: Columnar + PartialEq>(name: &'static str, c: &mut Criterion, data: &T) {
     let mut group = c.benchmark_group(format!("{}/columnar", name));
 
     let mut buffer = Vec::default();
@@ -35,32 +38,45 @@ pub fn bench<T: Columnar+PartialEq>(name: &'static str, c: &mut Criterion, data:
 
     group.bench_function("access", |b| {
         b.iter(|| {
-            let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(&mut Indexed::decode(&buffer));
+            let decoded =
+                <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(
+                    &mut Indexed::decode(&buffer),
+                );
             black_box(decoded);
         })
     });
 
     group.bench_function("deserialize", |b| {
         b.iter(|| {
-            let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(&mut Indexed::decode(&buffer));
+            let decoded =
+                <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(
+                    &mut Indexed::decode(&buffer),
+                );
             black_box(<T as Columnar>::into_owned(decoded.get(0)));
         })
     });
 
-    let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(&mut Indexed::decode(&buffer));
+    let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(
+        &mut Indexed::decode(&buffer),
+    );
     let mut deser: T = <T as Columnar>::into_owned(decoded.get(0));
 
     // Deserialize into an existing instance, to avoid benchmarking the allocator.
     group.bench_function("deserialize_opt", |b| {
         b.iter(|| {
-            let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(&mut Indexed::decode(&buffer));
+            let decoded =
+                <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(
+                    &mut Indexed::decode(&buffer),
+                );
             deser.copy_from(decoded.get(0));
             black_box(&deser);
         })
     });
     crate::bench_size(name, "columnar", bytemuck::cast_slice(&buffer));
 
-    let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(&mut Indexed::decode(&buffer));
+    let decoded = <<<T as Columnar>::Container as Borrow>::Borrowed<'_> as FromBytes>::from_bytes(
+        &mut Indexed::decode(&buffer),
+    );
     let deser: T = <T as Columnar>::into_owned(decoded.get(0));
     assert!(data == &deser);
 
